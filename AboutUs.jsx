489e9deb366864d630e2d684f70c9877cff2b07c
Arbinda-Sahu <arbindakumarsahu13@gmail.com>
2026-04-20 12:59:43 +0530

    Add AboutUs component with nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,21 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant shop that offers a wide variety of
+        house plants for homes and offices.
+      </p>
+      <p>
+        Our mission is to make gardening simple and enjoyable by providing
+        healthy plants, affordable prices, and excellent customer service.
+      </p>
+      <p>
+        We offer indoor plants, decorative plants, and gardening accessories
+        to help customers create a green and beautiful environment.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
